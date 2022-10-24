2d750f3375066eaff615de57a65e6ddaeece65a7
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-10-24 06:05:16 -0400

    Add timer code to login.js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,39 @@
+async function timer() {
+    var idleTime = 0;
+    document.ready(function () {
+        function timerIncrement() {
+            idleTime = idleTime + 1;
+            if (idleTime > 0) { // 1 minutes
+                // window.location.reload();
+                async function logout() {
+                    const response = await fetch('/api/users/logout', {
+                        method: 'post',
+                        headers: { 'Content-Type': 'application/json' }
+                    });
+                
+                    if (response.ok) {
+                        document.location.replace('/');
+                    } else {
+                        alert(response.statusText);
+                    }
+                }
+                logout;
+            }
+        }
+        // Increment the idle time counter every minute.
+        var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
+
+        // Zero the idle timer on mouse movement.
+        document.mousemove(function (e) {
+            idleTime = 0;
+        });
+
+        document.keypress(function (e) {
+            idleTime = 0;
+        });
+    })
+};
+
 async function loginFormHandler(event) {
     event.preventDefault();
 
@@ -16,6 +52,7 @@ async function loginFormHandler(event) {
 
         if (response.ok) {
             document.location.replace('/dashboard');
+            timer();
         } else {
             alert(response.statusText);
         }
@@ -43,6 +80,8 @@ async function signupFormHandler(event) {
         // check the response status
         if (response.ok) {
             console.log('success');
+            document.location.replace('/dashboard');
+            timer();
         } else {
             alert(response.statusText);
         }
--- utils/auth.js
+++ utils/auth.js
@@ -6,30 +6,6 @@ const withAuth = (req, res, next) => {
     }
 };
 
-const timer = () => {
-    var idleTime = 0;
-    $(document).ready(function () {
-        function timerIncrement() {
-            idleTime = idleTime + 1;
-            if (idleTime > 0) { // 1 minutes
-                // window.location.reload();
-                res.redirect('/login');
 
-            }
-        }
 
-        // Increment the idle time counter every minute.
-        var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
-
-        // Zero the idle timer on mouse movement.
-        $(this).mousemove(function (e) {
-            idleTime = 0;
-        });
-
-        $(this).keypress(function (e) {
-            idleTime = 0;
-        });
-    })
-};
-
-module.exports = { withAuth, timer };
+module.exports = withAuth;
