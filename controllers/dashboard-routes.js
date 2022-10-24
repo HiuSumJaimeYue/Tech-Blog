c5bc1df118c4419305428bae06457cd81560857b
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-10-24 05:08:42 -0400

    Add new routes and handlebar for add-post

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -41,6 +41,44 @@ router.get('/', withAuth, (req, res) => {
         });
 });
 
+router.get('/add', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            // use the ID from the session
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'content',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            // serialize data before passing to template
+            // const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('add-post', { loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 router.get('/edit/:id', withAuth, (req, res) => {
     Post.findByPk(req.params.id, {
         attributes: [
